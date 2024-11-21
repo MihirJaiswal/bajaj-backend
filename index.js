64eba6e8d99de513803605edb7f688d55554787d
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-11-21 16:50:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 // Middleware (if any, e.g., body-parser, CORS)
 app.use(express.json());
 
